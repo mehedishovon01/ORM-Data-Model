8f07606c5cd149ad428682a94ced8c013c56b1a2
mehedishovon01 <mehedishovon01@gmail.com>
2024-01-01 22:42:56 +0600

    Prepare for Railway Deploy

--- OrmDataModel/settings.py
+++ OrmDataModel/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-c@yhuds136_iou2)t3)^_d6)hkl6x(=c)8^1#aqq5if^(-%07$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -119,9 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (
-    BASE_DIR/'static',
-)
+# STATICFILES_DIRS = (
+#     BASE_DIR/'static',
+# )
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
