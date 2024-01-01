f641e2d5fa3b91523fda4fe17b3ad582fd5d6025
mehedishovon01 <mehedishovon01@gmail.com>
2024-01-01 19:15:47 +0600

    Settings

--- OrmDataModel/settings.py
+++ OrmDataModel/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,14 +119,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATICFILES_DIRS = (
-#     BASE_DIR/'static',
-# )
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR/ 'static/')]
-STATIC_ROOT =os.path.join(BASE_DIR/ 'staticfiles/')
+STATICFILES_DIRS = (
+    BASE_DIR/'static',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://orm-data-model-production.up.railway.app'
+]
