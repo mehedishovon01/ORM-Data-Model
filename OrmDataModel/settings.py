90d0a2aa06f229282bb256f68ed3d68b4b864620
mehedishovon01 <mehedishovon01@gmail.com>
2024-01-01 23:23:24 +0600

    MySQL Updated

--- OrmDataModel/settings.py
+++ OrmDataModel/settings.py
@@ -13,6 +13,14 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
+# import dj_database_url
+
+# DATABASE_URL = "mysql://root:bEBBG1AHgB5baag5FDh4CfccbG32AfEC@viaduct.proxy.rlwy.net:48492/railway"
+
+# DATABASES = {
+#     "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
+# }
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -81,9 +89,20 @@ WSGI_APPLICATION = 'OrmDataModel.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'railway',
+        'USER': 'root',  
+        'PASSWORD': 'bEBBG1AHgB5baag5FDh4CfccbG32AfEC',  
+        'HOST': 'viaduct.proxy.rlwy.net',  
+        'PORT': '48492',  
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+            }
     }
 }
 
